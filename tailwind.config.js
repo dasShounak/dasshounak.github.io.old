ee8ed29028bb931d2d1329832228e3c19c72e0eb
dasShounak <shounakdas19@gmail.com>
2021-06-17 09:38:54 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  mode: 'jit',
+  purge: {
+    enabled: true,
+    content: [
+      './*.html',
+      'pages/*.html',
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif']
+    },
+    colors: {
+      transparent: 'transparent',
+      white: colors.white,
+      black: colors.black,
+      gray: colors.gray,
+      highlight: '#FF3399',
+    },
+    extend: {
+      letterSpacing: {
+        sowide: '0.3rem',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
